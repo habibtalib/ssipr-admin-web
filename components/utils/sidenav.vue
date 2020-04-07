54e1ed05bf072d4d34feb8080a0b12a1e53fad6c
Habib Talib <cyberx11@gmail.com>
2020-04-07 11:49:16 +0800

    enable SADE

--- components/utils/sidenav.vue
+++ components/utils/sidenav.vue
@@ -24,13 +24,13 @@
           >
           </b-icon>
         </template>
-        <!-- <b-menu-item
+        <b-menu-item
           :active="currentPath == '/ipr/SADE'"
           icon="file-plus"
           label="Skim Air Selangor"
           to="/ipr/SADE"
           tag="nuxt-link"
-        ></b-menu-item> -->
+        ></b-menu-item>
         <!-- <b-menu-item
           :active="currentPath == '/ipr/KISS'"
           icon="file-plus"
